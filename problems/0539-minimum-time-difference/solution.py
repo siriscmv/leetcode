6822f67384f6bcf7cf913f276cef6375ee673186
Siris <40269790+Siris01@users.noreply.github.com>
2023-12-26 13:37:52 +0000

    [sync] Runtime - 69 ms (86.75%), Memory - 20.3 MB (25.17%)

---
+++ problems/0539-minimum-time-difference/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        for i, time in enumerate(timePoints):
+            timePoints[i] = int(time[:2]) * 60 + int(time[3:])
+
+        timePoints.sort()
+        timePoints.append(timePoints[0] + 24 * 60)
+        ans = float("inf")
+        for i in range(len(timePoints) - 1):
+            diff = min(
+                abs(timePoints[i + 1] - timePoints[i]),
+                24 * 60 - abs(timePoints[i + 1] - timePoints[i]),
+            )
+            if diff < ans:
+                ans = diff
+        return ans
+
+
