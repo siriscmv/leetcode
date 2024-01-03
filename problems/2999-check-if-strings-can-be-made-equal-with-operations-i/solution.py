1356e786918e610b1a0cf54a68f8a9e8512a732e
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-03 07:29:21 +0000

    [sync] Runtime - 33 ms (90.20%), Memory - 17.3 MB (23.34%)

---
+++ problems/2999-check-if-strings-can-be-made-equal-with-operations-i/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+      a = sorted(s1[0::2]) + sorted(s1[1::2])
+      b = sorted(s2[0::2]) + sorted(s2[1::2])
+      return a == b
+        
