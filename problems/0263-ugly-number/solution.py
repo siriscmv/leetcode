e6589d8e9b005cc3553dc297cf9de480d93b826c
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-04 10:27:49 +0000

    [sync] Runtime - 33 ms (87.21%), Memory - 17.4 MB (12.79%)

---
+++ problems/0263-ugly-number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+      while n > 1:
+        if n % 2 == 0: n //= 2
+        elif n % 3 == 0: n //= 3
+        elif n % 5 == 0: n //= 5
+        else: break
+
+      return n == 1
