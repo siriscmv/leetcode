1788168db6930ecd90b8035b50481902ab0554ea
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 11:09:49 +0000

    [sync] Runtime - 37 ms (90.15%), Memory - 17.4 MB (14.67%)

--- problems/1528-kids-with-the-greatest-number-of-candies/solution.py
+++ problems/1528-kids-with-the-greatest-number-of-candies/solution.py
@@ -2,4 +2,3 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         m = max(candies)
         return list(map(lambda c: c+extraCandies >= m,  candies))
-        
