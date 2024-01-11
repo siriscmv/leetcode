731b70534f19cc19323ee6bb7ff090b9d2f9cca0
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 12:39:11 +0000

    [sync] Runtime - 133 ms (81.82%), Memory - 18.8 MB (23.14%)

--- problems/0283-move-zeroes/solution.py
+++ problems/0283-move-zeroes/solution.py
@@ -1,9 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        curr = len(nums)-1
+        curr = len(nums) - 1
         while curr >= 0:
             if nums[curr] == 0:
                 del nums[curr]
