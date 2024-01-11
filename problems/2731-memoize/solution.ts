b4d4822b97407449ea7194faf797e748e434e2c2
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 08:32:10 +0000

    [sync] Runtime - 290 ms (57.64%), Memory - 110.2 MB (24.28%)

--- problems/2731-memoize/solution.ts
+++ problems/2731-memoize/solution.ts
@@ -2,25 +2,13 @@ type Fn = (...params: number[]) => number
 
 function memoize(fn: Fn): Fn {
     const cache = {};
+
     return function(...args) {
-        const key = args.join('|')
+        const key = args.join('#')
         const res = cache[key];
         if (res !== undefined) return res;
 
-        const accRes = fn(...args);
-        cache[key] = accRes;
-        return accRes;
+        cache[key] = fn(...args);
+        return cache[key];
     }
 }
-
-
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
