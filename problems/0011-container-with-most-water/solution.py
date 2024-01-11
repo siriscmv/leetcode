68173f77187dedc905afa593db0b013d3c03a0ad
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 12:38:49 +0000

    [sync] Runtime - 536 ms (85.60%), Memory - 29.9 MB (34.54%)

--- problems/0011-container-with-most-water/solution.py
+++ problems/0011-container-with-most-water/solution.py
@@ -6,11 +6,11 @@ class Solution:
         while i < j:
             vol = (j-i) * min(height[i], height[j])
             ans = max(ans, vol)
-            if height[i] > height[j]: j-=1
-            elif height[i] < height[j]: i+=1
+            if height[i] > height[j]: j -= 1
+            elif height[i] < height[j]: i += 1
             else:
-                i+=1
-                j-=1
+                i += 1
+                j -= 1
         return ans
             
         
