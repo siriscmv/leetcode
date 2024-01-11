511d91e1e1ea8b21be7440414990b4dc5d97e6fa
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 08:39:09 +0000

    [sync] Runtime - 67 ms (12.30%), Memory - 42.4 MB (76.74%)

--- problems/2733-sleep/solution.ts
+++ problems/2733-sleep/solution.ts
@@ -1,11 +1,3 @@
 async function sleep(millis: number): Promise<void> {
-    return new Promise(resolve => {
-        setTimeout((resolve), millis)
-    })
+    return new Promise(resolve => setTimeout((resolve), millis))
 }
-
-
-/** 
- * let t = Date.now()
- * sleep(100).then(() => console.log(Date.now() - t)) // 100
- */
