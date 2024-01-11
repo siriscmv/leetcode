5c0c235b85d2de683541380c1a34ce34aca20943
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 08:30:36 +0000

    [sync] Runtime - 59 ms (36.80%), Memory - 44.4 MB (5.06%)

--- problems/2747-apply-transform-over-each-element-in-array/solution.ts
+++ problems/2747-apply-transform-over-each-element-in-array/solution.ts
@@ -1,11 +1,5 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
     const ans = [];
-    let i = 0;
-
-    for (const num of arr) {
-      ans.push(fn(num, i));
-      i++;
-    }
-    
+    for (const [i, n] of arr.entries()) ans.push(fn(n, i));
     return ans;
 };
