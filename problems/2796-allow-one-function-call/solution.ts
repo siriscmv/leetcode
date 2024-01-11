5b76e566ae03c3e1a40521d55e4c8ffa29181a68
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 08:33:29 +0000

    [sync] Runtime - 51 ms (82.81%), Memory - 42.9 MB (46.14%)

---
+++ problems/2796-allow-one-function-call/solution.ts
@@ -0,0 +1,13 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let called = false;
+    return function (...args) {
+        if (called) return undefined;
+        else {
+            called = true;
+            return fn(...args)
+        }
+    };
+}
