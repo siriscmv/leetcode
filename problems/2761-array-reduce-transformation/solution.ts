e4abb1000bd7c87f8c649d0383f4709b566217f4
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 08:28:13 +0000

    [sync] Runtime - 65 ms (25.02%), Memory - 45.3 MB (5.52%)

---
+++ problems/2761-array-reduce-transformation/solution.ts
@@ -0,0 +1,7 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    let curr = init;
+    for (const num of nums) curr = fn(curr, num);
+    return curr;
+};
