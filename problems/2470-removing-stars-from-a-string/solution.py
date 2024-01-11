3d3155884d3306ba75cf5b218fc7240721ed1cb6
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-11 18:56:09 +0000

    [sync] Runtime - 126 ms (96.11%), Memory - 19 MB (28.61%)

---
+++ problems/2470-removing-stars-from-a-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for ch in s:
+            if ch == '*': stack.pop()
+            else: stack.append(ch)
+        
+        return "".join(stack)
