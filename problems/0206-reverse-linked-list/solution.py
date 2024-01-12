a741d6a4cee4bea5fc25921edaa2d616c420bdb8
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-12 05:42:27 +0000

    [sync] Runtime - 26 ms (99.45%), Memory - 18.6 MB (18.50%)

--- problems/0206-reverse-linked-list/solution.py
+++ problems/0206-reverse-linked-list/solution.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr, prev = head, None
@@ -13,4 +7,3 @@ class Solution:
             prev = curr
             curr = n
         return prev
-
