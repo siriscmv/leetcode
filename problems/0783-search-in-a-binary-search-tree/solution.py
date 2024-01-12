6eacd25d1797f440e4aeb62d74f77f768ddf582f
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-12 11:59:41 +0000

    [sync] Runtime - 58 ms (84.53%), Memory - 19.1 MB (12.21%)

---
+++ problems/0783-search-in-a-binary-search-tree/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return None
+        if val < root.val: return self.searchBST(root.left, val)
+        if val > root.val: return self.searchBST(root.right, val)
+        return root
