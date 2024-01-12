f2d2ba28465d35cd955662b422cac1dd17e45a91
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-12 01:59:15 +0000

    [sync] Runtime - 40 ms (67.94%), Memory - 17.2 MB (37.93%)

--- problems/1823-determine-if-string-halves-are-alike/solution.py
+++ problems/1823-determine-if-string-halves-are-alike/solution.py
@@ -1,10 +1,10 @@
 class Solution:
-    VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    VOWELS = ['a', 'e', 'i', 'o', 'u']
     def halvesAreAlike(self, s: str) -> bool:
-        a, b, mid = 0, 0, len(s)/2
+        a, b, mid = 0, 0, len(s) // 2
 
         for i, char in enumerate(s):
-            if char in self.VOWELS:
+            if char.lower() in self.VOWELS:
                 if i < mid: a += 1
                 else: b += 1
         
