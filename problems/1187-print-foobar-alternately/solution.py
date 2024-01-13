9f9d6489da6956bcc5eb214e08ede65cd5c7c671
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-13 16:09:23 +0000

    [sync] Runtime - 65 ms (79.31%), Memory - 17.6 MB (21.75%)

---
+++ problems/1187-print-foobar-alternately/solution.py
@@ -0,0 +1,17 @@
+from threading import Barrier
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.barrier = Barrier(2)
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            printFoo()
+            self.barrier.wait()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.barrier.wait()
+            printBar()
+            self.barrier.reset()
