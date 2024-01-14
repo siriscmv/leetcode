ad1146eb21d3a1ba11e0b68d0f20853eeb3e236b
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-14 02:33:04 +0000

    [sync] Runtime - 47 ms (100.00%), Memory - 17.3 MB (100.00%)

---
+++ problems/3242-count-elements-with-maximum-frequency/solution.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        if not nums: return 0
+        
+        counts = defaultdict(int)
+        for n in nums: counts[n] += 1
+        
+        maxx = max(counts.values())
+        return sum([n for n in counts.values() if n == maxx])
