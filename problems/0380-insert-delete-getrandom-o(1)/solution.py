457f64a85343c2fb13083e7bdd81ce30955c69c1
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-16 05:23:34 +0000

    [sync] Runtime - 252 ms (95.10%), Memory - 64.5 MB (27.57%)

---
+++ problems/0380-insert-delete-getrandom-o(1)/solution.py
@@ -0,0 +1,26 @@
+from random import choice
+
+class RandomizedSet:
+    def __init__(self):
+        self.map = {}
+        self.list = list()
+
+    def insert(self, val: int) -> bool:
+        if val in self.map: return False
+
+        self.list.append(val)
+        self.map[val] = len(self.list) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map: return False
+
+        i = self.map[val]
+        del self.map[val]
+        self.list[i] = self.list[-1]
+        if i < len(self.list) - 1: self.map[self.list[-1]] = i
+        self.list.pop()
+        return True
+
+    def getRandom(self) -> int:
+        return choice(self.list)
