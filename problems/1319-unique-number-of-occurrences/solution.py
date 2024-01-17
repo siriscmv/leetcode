3fe6471c5670813f52fe061fb18ceeb9bd271b15
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-17 02:12:47 +0000

    [sync] Runtime - 38 ms (80.04%), Memory - 17.5 MB (39.52%)

--- problems/1319-unique-number-of-occurrences/solution.py
+++ problems/1319-unique-number-of-occurrences/solution.py
@@ -2,9 +2,11 @@ from collections import defaultdict
 
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        counts = defaultdict(int)
+        counts, seen = defaultdict(int), set()
         
         for num in arr: counts[num] += 1
-        freq = counts.values()
+        for val in counts.values():
+            if val in seen: return False
+            seen.add(val)
 
-        return len(freq) == len(set(freq)) 
+        return True
