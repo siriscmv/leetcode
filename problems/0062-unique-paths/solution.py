9e9c8c6723299d1b119ba825aee62fc2e4ba963b
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-19 16:57:10 +0000

    [sync] Runtime - 32 ms (88.69%), Memory - 16.7 MB (55.54%)

---
+++ problems/0062-unique-paths/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = {}
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m-1 or j == n-1: dp[(i, j)] = 1
+                else: dp[(i, j)] = dp[(i+1, j)] + dp[(i, j+1)]
+        
+        return dp[(0, 0)]
