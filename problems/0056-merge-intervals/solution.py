44423a3f91326e5b54efb3e2933e5577dd62ecec
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-21 14:55:40 +0000

    [sync] Runtime - 118 ms (92.35%), Memory - 20.5 MB (98.32%)

---
+++ problems/0056-merge-intervals/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans, l = [intervals[0]], len(intervals)
+
+        for i in range(1, l):
+            if intervals[i][0] <= ans[-1][1]: ans[-1][1] = max(ans[-1][1], intervals[i][1])
+            else: ans.append(intervals[i])
+        
+        return ans
