82d6c49a75117b29b43fa84bb7bcd1bfdeff1854
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-21 00:54:06 +0000

    [sync] Runtime - 29 ms (96.15%), Memory - 16.6 MB (56.96%)

--- problems/0198-house-robber/solution.py
+++ problems/0198-house-robber/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        for i in range(1, len(nums)):
-            if i == 1: nums[i] = max(nums[i-1], nums[i])
-            else: nums[i] = max(nums[i-1], nums[i] + nums[i-2])
-        
         if len(nums) == 1: return nums[0]
+        nums[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i-1], nums[i] + nums[i-2])
+        
         return max(nums[-1], nums[-2])
