040c96ecf14ee1583a3ec1b35c39e21168da7833
Siris <40269790+Siris01@users.noreply.github.com>
2024-01-30 01:13:02 +0000

    [sync] Runtime - 61 ms (88.71%), Memory - 17.1 MB (71.20%)

--- problems/0150-evaluate-reverse-polish-notation/solution.py
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,11 +1,18 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        m = {'+': lambda a, b: a+b, '-': lambda a, b: a-b, '*': lambda a, b: a*b, '/': lambda a, b: int(a/b)}
+        ops = {
+            '+': lambda a, b: a+b,
+            '-': lambda a, b: a-b,
+            '*': lambda a, b: a*b,
+            '/': lambda a, b: int(a/b)
+        }
+
         for token in tokens:
-            if token not in m: stack.append(int(token))
+            if token not in ops: stack.append(int(token))
             else:
                 a = stack.pop()
                 b = stack.pop()
-                stack.append(m[token](b, a))
+                stack.append(ops[token](b, a))
+        
         return round(stack.pop())
