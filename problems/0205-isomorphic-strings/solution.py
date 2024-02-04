ca0d23415447d2436898a4baf40645b6ca465e37
Siris <40269790+Siris01@users.noreply.github.com>
2024-02-04 07:27:05 +0000

    [sync] Runtime - 36 ms (90.68%), Memory - 16.8 MB (73.53%)

--- problems/0205-isomorphic-strings/solution.py
+++ problems/0205-isomorphic-strings/solution.py
@@ -1,18 +1,14 @@
 class Solution:
-    def getForm(self, s: str):
-        form, seen = [], {}
-        i, l = 0, len(s)
-        while i < l:
-            j = i+1
-            while j < l and  s[j] == s[i]: j += 1
-            char = s[i]
-            if char in seen: form.append((j-i, seen[char])) 
-            else:
-                seen[char] = len(form)
-                form.append((j-i, None))
-            i = j
-        return form
-
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        return all(a == b for a, b in zip(self.getForm(s), self.getForm(t)))
+        mp, seen, l = {}, set(), len(s)
+        if l != len(t): return False
+
+        for i in range(l):
+            if s[i] not in mp: 
+                if t[i] in seen: return False
+                else: 
+                    mp[s[i]] = t[i]
+                    seen.add(t[i])
+            elif mp[s[i]] != t[i]: return False
+        
+        return True
