e4aa4afcbcd109a63c90bd9b8b290c88b3728989
Siris <40269790+Siris01@users.noreply.github.com>
2024-02-12 02:53:06 +0000

    [sync] Runtime - 129 ms (86.26%), Memory - 18 MB (63.93%)

--- problems/0169-majority-element/solution.py
+++ problems/0169-majority-element/solution.py
@@ -2,11 +2,11 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         ans, c = None, 0
 
-        for i in range(len(nums)):
+        for num in nums:
             if not c: 
-                ans = nums[i]
+                ans = num
                 c = 1
-            elif ans == nums[i]: c += 1
+            elif ans == num: c += 1
             else: c -= 1
         
         return ans
