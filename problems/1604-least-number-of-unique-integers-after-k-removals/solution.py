91f424afa14710a0ed7826da2f0c7356429e21cd
Siris <40269790+Siris01@users.noreply.github.com>
2024-02-16 03:49:41 +0000

    [sync] Runtime - 339 ms (68.75%), Memory - 33.3 MB (94.30%)

---
+++ problems/1604-least-number-of-unique-integers-after-k-removals/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        counts = defaultdict(int)
+        for num in arr: counts[num] += 1
+
+        counts = sorted(counts.values())
+        i, l = 0, len(counts)
+        
+        while k:
+            if k < counts[i]: break
+
+            k -= counts[i]
+            i += 1
+        
+        return l - i
