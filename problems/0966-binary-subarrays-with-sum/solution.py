3b1a0c89d077e917ec8c1268ee51926d46830434
Siris <40269790+Siris01@users.noreply.github.com>
2024-03-14 16:00:44 +0000

    [sync] Runtime - 662 ms (5.15%), Memory - 18.5 MB (68.90%)

---
+++ problems/0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        ans, sums = 0, deque()
+        curr, l, target = 0, len(nums), goal
+
+        for n in nums:
+            sums.append(curr + n)
+            curr += n
+        
+        for _ in range(l):
+            a = bisect.bisect_left(sums, target)
+            b = bisect.bisect_right(sums, target)
+
+            ans += b - a
+            target = goal + sums.popleft()
+        
+        return ans
