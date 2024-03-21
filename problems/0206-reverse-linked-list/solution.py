8aa1a25aa665684fd856bea030c7da6ae741174d
Siris <40269790+Siris01@users.noreply.github.com>
2024-03-21 01:48:54 +0000

    [sync] Runtime - 31 ms (90.72%), Memory - 17.7 MB (76.82%)

--- problems/0206-reverse-linked-list/solution.py
+++ problems/0206-reverse-linked-list/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr, prev = head, None
+        
         while curr:
-            n = curr.next
+            nxt = curr.next
             curr.next = prev
             prev = curr
-            curr = n
+            curr = nxt
+        
         return prev
